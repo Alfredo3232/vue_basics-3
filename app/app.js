26705ef049bc1298e6623304ba7238d37ecce648
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-09-29 09:01:16 -0700

    add reset input func & removed some code

--- app/app.js
+++ app/app.js
@@ -6,14 +6,17 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+    setName(event) {
+      this.name = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
     },
     reduce(num) {
       this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.name = '';
     }
   }
 });
