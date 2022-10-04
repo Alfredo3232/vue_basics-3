681bd39c10ab6862d237a3ea0f1a167e5d1bea6e
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-04 11:40:26 -0500

    implement functionality to make divs 'activate'

--- app/app.js
+++ app/app.js
@@ -10,10 +10,16 @@ const app = Vue.createApp({
         boxSelected(box) {
             if (box === 'A') {
                 this.boxASelected = true;
+
+                console.log("selected A");
             } else if (box === 'B') {
                 this.boxBSelected = true;
+
+                console.log("selected B");
             } else if (box === 'C') {
                 this.boxCSelected = true;
+
+                console.log("selected C");
             }
         }
     }
