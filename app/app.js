98582b76979430a0b4d5d3f3bab02bbe19df54f3
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-10-04 13:03:28 -0500

    add linter changes and add prettier

--- app/app.js
+++ app/app.js
@@ -1,28 +1,28 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false
-        }
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = true;
+
+        console.log("selected A");
+      } else if (box === "B") {
+        this.boxBSelected = true;
+
+        console.log("selected B");
+      } else if (box === "C") {
+        this.boxCSelected = true;
+
+        console.log("selected C");
+      }
     },
-    methods: {
-        boxSelected(box) {
-            if (box === 'A') {
-                this.boxASelected = true;
+  },
+});
 
-                console.log("selected A");
-            } else if (box === 'B') {
-                this.boxBSelected = true;
-
-                console.log("selected B");
-            } else if (box === 'C') {
-                this.boxCSelected = true;
-
-                console.log("selected C");
-            }
-        }
-    }
-})
-
-app.mount("#styling")
+app.mount("#styling");
